c617ade37b071b078fe2029067c36492373ed5cd
Thanh Tùng <hathanhtung2202@gmail.com>
2024-09-09 23:52:54 +0700

    crud application

--- eShopSolution.Application/Catelog/Products/Dtos/ProductViewModel.cs
+++ eShopSolution.Application/Catelog/Products/Dtos/ProductViewModel.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace eShopSolution.Data.Entities
+namespace eShopSolution.Application.Catelog.Products.Dtos
 {
-    public class ProductTranslation
+    public class ProductViewModel
     {
-        public int Id { set; get; }
-        public int ProductId { set; get; }
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreate { get; set; }
         public string Name { set; get; }
         public string Description { set; get; }
         public string Details { set; get; }
@@ -15,8 +19,5 @@ namespace eShopSolution.Data.Entities
         public string SeoTitle { set; get; }
         public string SeoAlias { get; set; }
         public string LanguageId { set; get; }
-        public Product Product { get; set; }
-        public Language Language { get; set; }
-
     }
 }
---
+++ eShopSolution.Application/Catelog/Products/IManagerProductService.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Application.Catelog.Products.Dtos;
+using eShopSolution.Application.Catelog.Products.Dtos.Manage;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catelog.Products
+{
+    public interface IManagerProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+        Task<int> Update(ProductUpdateRequest request);
+        Task<int> Delete(int productId);
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task<bool> UpdatePrice(int productId,  double newPrice);
+        Task<bool> UpdateStock(int productId,  int addedQuantity);
+        public Task AddViewcount(int productId);
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+    }
+}
