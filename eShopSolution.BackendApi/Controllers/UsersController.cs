4686c9c67666f60b4ea8ccf47a109622071328dd
Thanh Tùng <hathanhtung2202@gmail.com>
2024-10-23 22:52:26 +0700

    gán quyền cho user

--- eShopSolution.BackendApi/Controllers/UsersController.cs
+++ eShopSolution.BackendApi/Controllers/UsersController.cs
@@ -87,5 +87,19 @@ namespace eShopSolution.BackendApi.Controllers
             var result = await _userService.Delete(id);
             return Ok(result);
         }
+
+        [HttpPut("{id}/roles")]
+        public async Task<IActionResult> RoleAssign(Guid id, [FromBody] RoleAssignRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _userService.RoleAssign(id, request);
+            if (!result.IsSuccessed)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
     }
 }
