e99bcc9424e9416fdd71cbd0e2c2f7d3e41c742c
Thanh Tùng <hathanhtung2202@gmail.com>
2024-10-24 14:59:22 +0700

    B2: add component

--- eShopSolution.AdminApp/Controllers/Components/NavigationViewComponent.cs
+++ eShopSolution.AdminApp/Controllers/Components/NavigationViewComponent.cs
@@ -1,4 +1,6 @@
 ﻿using eShopSolution.AdminApp.Models;
+using eShopSolution.AdminApp.Services;
+using eShopSolution.Utilities.Constants;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -7,25 +9,25 @@ namespace eShopSolution.AdminApp.Controllers.Components
 {
     public class NavigationViewComponent : ViewComponent
     {
-        //private readonly ILanguageApiClient _languageApiClient;
+        private readonly ILanguageApiClient _languageApiClient;
 
-        //public NavigationViewComponent(ILanguageApiClient languageApiClient)
-        //{
-        //    _languageApiClient = languageApiClient;
-        //}
+        public NavigationViewComponent(ILanguageApiClient languageApiClient)
+        {
+            _languageApiClient = languageApiClient;
+        }
 
-        //public async Task<IViewComponentResult> InvokeAsync()
-        //{
-        //    var languages = await _languageApiClient.GetAll();
-        //    var navigationVm = new NavigationViewModel()
-        //    {
-        //        CurrentLanguageId = HttpContext
-        //        .Session
-        //        .GetString(SystemConstants.AppSettings.DefaultLanguageId),
-        //        Languages = languages.ResultObj
-        //    };
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var languages = await _languageApiClient.GetAll();
+            var navigationVm = new NavigationViewModel()
+            {
+                CurrentLanguageId = HttpContext
+                .Session
+                .GetString(SystemConstants.AppSettings.DefaultLanguageId),
+                Languages = languages.ResultObj
+            };
 
-        //    return View("Default", navigationVm);
-        //}
+            return View("Default", navigationVm);
+        }
     }
 }
